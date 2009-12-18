84724b0126863d6225ba63167019c5651ed51a93
Fred Wenzel <fwenzel@mozilla.com>
2009-12-18 09:35:23 -0800

    making admin panel the front page (for now)

--- urls.py
+++ urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    #(r'^admin/', include(admin.site.urls)),
+    (r'^', include(admin.site.urls)),
 )
