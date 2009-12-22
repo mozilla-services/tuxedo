eaf7971659b08b6983c74d574d38250e6541187b
Fred Wenzel <fwenzel@mozilla.com>
2009-12-22 07:48:36 -0800

    admin panel for GeoIP entities

---
+++ apps/geoip/admin.py
@@ -0,0 +1,23 @@
+from geoip.models import Country, IPBlock, Region
+from django.contrib import admin
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('country_name', 'country_code', 'continent', 'region')
+    list_filter = ('continent', 'region')
+    ordering = ('country_name',)
+admin.site.register(Country, CountryAdmin)
+
+class IPBlockAdmin(admin.ModelAdmin):
+    list_display = ('ip_start_addr', 'ip_start', 'ip_end_addr', 'ip_end',
+                    'country')
+    list_filter = ('country',)
+    ordering = ('ip_start',)
+admin.site.register(IPBlock, IPBlockAdmin)
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'priority', 'throttle')
+    list_filter = ('priority',)
+    ordering = ('name',)
+admin.site.register(Region, RegionAdmin)
+
