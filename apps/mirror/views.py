23fcfa872028459a5371850a7a273dfa32ddc97f
Fred Wenzel <fwenzel@mozilla.com>
2010-01-05 05:50:59 -0800

    initial views: uptake and index

--- apps/mirror/views.py
+++ apps/mirror/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def index(request):
+    """Main login/index page"""
+    return HttpResponse('OMG')
+
+def uptake(request):
+    """Show mirror uptake"""
+    return HttpResponse('OMG')
+
