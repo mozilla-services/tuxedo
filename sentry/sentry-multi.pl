4da1939d8bb649ac9ee3d9c651ef1dc91c249c70
Fred Wenzel <fwenzel@mozilla.com>
2010-01-13 10:12:49 -0800

    using simplified column names in sentry (bug 538988)

--- sentry/sentry-multi.pl
+++ sentry/sentry-multi.pl
@@ -36,12 +36,12 @@ my $dbh = DBI->connect( "DBI:mysql:$db:$host",$user,$pass) or die "Connecting : 
 my $checknow = "";
 if (defined($ARGV[0]) and $ARGV[0] eq 'checknow') {
     $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products
-    ON mirror_locations.product_id = mirror_products.product_id WHERE
-    product_active='1' AND product_checknow=1};
+    ON mirror_locations.product_id = mirror_products.id WHERE
+    mirror_products.active='1' AND mirror_products.checknow=1};
 } else {
-    $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products ON mirror_locations.product_id = mirror_products.product_id WHERE product_active='1'};
+    $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products ON mirror_locations.product_id = mirror_products.id WHERE mirror_products.active='1'};
 }
-$mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE mirror_active='1' ORDER BY mirror_name};
+$mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE active='1' ORDER BY name};
 
 my $location_sth = $dbh->prepare($location_sql);
 my $mirror_sth = $dbh->prepare($mirror_sql);
@@ -57,6 +57,6 @@ while (my $location = $location_sth->fetchrow_hashref() ) {
 $mirror_sth->execute();
 
 while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
-    system ("/usr/bin/perl -I/data/sentry /data/sentry/sentry.pl checknow " . $mirror->{mirror_id} . " &");
+    system ("/usr/bin/perl -I/data/sentry /data/sentry/sentry.pl checknow " . $mirror->{id} . " &");
     sleep 1; # wait a second between each spawn so we don't hose the system by spawning 150 at once
 }
--- sentry/sentry.pl
+++ sentry/sentry.pl
@@ -52,27 +52,27 @@ sub log_this {
 my $dbh = DBI->connect( "DBI:mysql:$db:$host",$user,$pass) or die "Connecting : $dbi::errstr\n";
 if (defined($ARGV[0]) and $ARGV[0] eq 'checknow') {
     $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products
-    ON mirror_locations.product_id = mirror_products.product_id WHERE
-    product_active='1' AND product_checknow=1};
+    ON mirror_locations.product_id = mirror_products.id WHERE
+    mirror_products.active='1' AND mirror_products.checknow=1};
     if (defined($ARGV[1])) {
         if ($ARGV[1] =~ /^\d+$/) {
-            $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE mirror_active='1' AND mirror_id=$ARGV[1]};
+            $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE active='1' AND id=$ARGV[1]};
         } else {
-            $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE mirror_active='1' AND (mirror_baseurl LIKE } . $dbh->quote('%'.$ARGV[1].'%') . qq{ OR mirror_name LIKE } . $dbh->quote('%'.$ARGV[1].'%') . ")";
+            $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE active='1' AND (baseurl LIKE } . $dbh->quote('%'.$ARGV[1].'%') . qq{ OR name LIKE } . $dbh->quote('%'.$ARGV[1].'%') . ")";
         }
     } else {
-        $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE mirror_active='1' ORDER BY mirror_name};
+        $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE active='1' ORDER BY name};
     }
 } else {
-    $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products ON mirror_locations.product_id = mirror_products.product_id WHERE product_active='1'};
-    $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE mirror_active='1' ORDER BY mirror_name};
+    $location_sql = qq{SELECT * FROM mirror_locations INNER JOIN mirror_products ON mirror_locations.product_id = mirror_products.id WHERE mirror_products.active='1'};
+    $mirror_sql = qq{SELECT * FROM mirror_mirrors WHERE active='1' ORDER BY name};
 }
-$update_sql = qq{REPLACE mirror_location_mirror_map SET location_id=?,mirror_id=?,location_active=?};
-$failed_mirror_sql = qq{UPDATE mirror_location_mirror_map SET location_active='0' WHERE mirror_id=?};
+$update_sql = qq{REPLACE mirror_location_mirror_map SET location_id=?,mirror_id=?,active=?};
+$failed_mirror_sql = qq{UPDATE mirror_location_mirror_map SET active='0' WHERE mirror_id=?};
 $log_sql = qq{INSERT INTO sentry_log (log_date, mirror_id, mirror_active, mirror_rating, reason) VALUES (FROM_UNIXTIME(?), ?, ?, ?, ?)};
 $getlog_sql = qq{SELECT mirror_rating, mirror_active FROM sentry_log WHERE mirror_id = ? ORDER BY log_date DESC LIMIT 4};
 $updatelog_sql = qq{UPDATE sentry_log SET reason=? WHERE log_date=FROM_UNIXTIME(?) AND mirror_id=?};
-$updaterating_sql = qq{UPDATE mirror_mirrors SET mirror_rating = ? WHERE mirror_id = ?};
+$updaterating_sql = qq{UPDATE mirror_mirrors SET rating = ? WHERE id = ?};
 
 my $location_sth = $dbh->prepare($location_sql);
 my $mirror_sth = $dbh->prepare($mirror_sql);
@@ -93,14 +93,14 @@ if ( $DEBUG ) {
 	$product_sth->execute();
 
 	while ( my $product = $product_sth->fetchrow_hashref() ) {
-		$products{$product->{product_id}} = $product->{product_name};
+		$products{$product->{id}} = $product->{name};
 	}
 
 	$oss_sth = $dbh->prepare($oss_sql);
         $oss_sth->execute();
 
         while ( my $os = $oss_sth->fetchrow_hashref() ) {
-                $oss{$os->{os_id}} = $os->{os_name};
+                $oss{$os->{id}} = $os->{name};
         }
 }
 
@@ -118,7 +118,7 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
 	my $ok = 1;
 
     # parse domain out of the mirror's base uri
-    my $domain = URI->new($mirror->{mirror_baseurl})->authority;
+    my $domain = URI->new($mirror->{baseurl})->authority;
 
     # test the domain, and mark the mirror as invalid on failure to resolve
     # if the mirror is bad, we should skip to the next mirror and avoid iterating over locations
@@ -127,15 +127,15 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
     # since for some reason Net::DNS::query still tries to resolve IP addresses (dumb).
     if ($domain !~ m($ipregex) && !$netres->query($domain)) {
         log_this "DNS resolution for $mirror->{mirror_name} FAILED!  Moving on to next mirror.\n";
-        $failed_mirror_sth->execute($mirror->{mirror_id});
-        $log_sth->execute($start_timestamp, $mirror->{mirror_id}, '0', $mirror->{mirror_rating}, "DNS failed");
+        $failed_mirror_sth->execute($mirror->{id});
+        $log_sth->execute($start_timestamp, $mirror->{id}, '0', $mirror->{rating}, "DNS failed");
 
         # send email to infra
 #        open(SENDMAIL, "|/usr/sbin/sendmail -t") or die "Cannot open /usr/sbin/sendmail: $!";
-#        print SENDMAIL "Subject: [bouncer] (" . $mirror->{mirror_id} . ") " . $mirror->{mirror_name} . " (weight: " . $mirror->{mirror_rating} . ") has failed DNS resolution\n";
+#        print SENDMAIL "Subject: [bouncer] (" . $mirror->{id} . ") " . $mirror->{name} . " (weight: " . $mirror->{rating} . ") has failed DNS resolution\n";
 #        print SENDMAIL "To: $email\n";
 #        print SENDMAIL "Content-type: text/plain\n\n";
-#        print SENDMAIL "$mirror->{mirror_name} failed DNS resolution.  All files for this mirror will be disabled until the next check.";
+#        print SENDMAIL "$mirror->{name} failed DNS resolution.  All files for this mirror will be disabled until the next check.";
 #        close(SENDMAIL);
 
         next;
@@ -143,20 +143,20 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
 
     # test the root of the domain, and mark the mirror as invalid on failure to find anything at root
     # we do not allow simple_request because the root of a mirror could return a redirect
-	my $mirrorReq = HTTP::Request->new(HEAD => $mirror->{mirror_baseurl});
+	my $mirrorReq = HTTP::Request->new(HEAD => $mirror->{baseurl});
     my $mirrorRes = $ua->request($mirrorReq);
 
     # if the mirror is bad, we should skip to the next mirror and avoid iterating over locations
     if ( $mirrorRes->{_rc}>=500 ) {
         if ( $mirrorRes->{_rc} == 500 ) {
-            log_this "$mirror->{mirror_baseurl} sent no response after " . $ua->timeout() . " seconds!  Checking recent history...\n";
-            $failed_mirror_sth->execute($mirror->{mirror_id});
-            $log_sth->execute($start_timestamp, $mirror->{mirror_id}, '0', $mirror->{mirror_rating}, "No response");
-            $getlog_sth->execute($mirror->{mirror_id});
-            my ($prevweight, $prevactive) = ($mirror->{mirror_rating}, 1);
+            log_this "$mirror->{baseurl} sent no response after " . $ua->timeout() . " seconds!  Checking recent history...\n";
+            $failed_mirror_sth->execute($mirror->{id});
+            $log_sth->execute($start_timestamp, $mirror->{id}, '0', $mirror->{rating}, "No response");
+            $getlog_sth->execute($mirror->{id});
+            my ($prevweight, $prevactive) = ($mirror->{rating}, 1);
             my $dropit = 1;
             while (my ($weight, $active) = $getlog_sth->fetchrow_array) {
-                log_this "**** weight $weight active $active for $mirror->{mirror_baseurl}\n";
+                log_this "**** weight $weight active $active for $mirror->{baseurl}\n";
                 if (($prevweight != $weight) || ($prevactive == $active)) {
                     $dropit = 0;
                 }
@@ -165,29 +165,29 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
             }
             my $newweight;
             if ($dropit) {
-                log_this "**** $mirror->{mirror_baseurl} Weight Drop Pattern matched, weight will be dropped 10%\n";
-                $newweight = $mirror->{mirror_rating} - int($mirror->{mirror_rating} * 0.10);
-                log_this "**** $mirror->{mirror_baseurl} Weight change $mirror->{mirror_rating} -> $newweight\n";
-                $updaterating_sth->execute($newweight, $mirror->{mirror_id});
+                log_this "**** $mirror->{baseurl} Weight Drop Pattern matched, weight will be dropped 10%\n";
+                $newweight = $mirror->{rating} - int($mirror->{rating} * 0.10);
+                log_this "**** $mirror->{baseurl} Weight change $mirror->{rating} -> $newweight\n";
+                $updaterating_sth->execute($newweight, $mirror->{id});
             } else {
                 log_this "Pattern OK, leaving weight unchanged.\n";
             }
         }
         else {
-            log_this "$mirror->{mirror_baseurl} returned error " . $mirrorRes->{_rc} . ".\n";
-            $failed_mirror_sth->execute($mirror->{mirror_id});
-            $log_sth->execute($start_timestamp, $mirror->{mirror_id}, '0', $mirror->{mirror_rating}, "Bad response");
+            log_this "$mirror->{baseurl} returned error " . $mirrorRes->{_rc} . ".\n";
+            $failed_mirror_sth->execute($mirror->{id});
+            $log_sth->execute($start_timestamp, $mirror->{id}, '0', $mirror->{rating}, "Bad response");
         }
 
-        $updatelog_sth->execute($output, $start_timestamp, $mirror->{mirror_id});
+        $updatelog_sth->execute($output, $start_timestamp, $mirror->{id});
         # send email to infra
  #       open(SENDMAIL, "|/usr/sbin/sendmail -t") or die "Cannot open /usr/sbin/sendmail: $!";
- #       print SENDMAIL "Subject: [bouncer] (" . $mirror->{mirror_id} . ") " . $mirror->{mirror_name} . " (weight: " . $mirror->{mirror_rating} . ") is not responding";
+ #       print SENDMAIL "Subject: [bouncer] (" . $mirror->{id} . ") " . $mirror->{name} . " (weight: " . $mirror->{rating} . ") is not responding";
  #       print SENDMAIL " - weight dropped to $newweight" if $dropit;
  #       print SENDMAIL "\n";
  #       print SENDMAIL "To: $email\n";
  #       print SENDMAIL "Content-type: text/plain\n\n";
- #       print SENDMAIL "$mirror->{mirror_name} sent no response for its URI: $mirror->{mirror_baseurl}.  All files for this mirror will be disabled until the next check.";
+ #       print SENDMAIL "$mirror->{name} sent no response for its URI: $mirror->{baseurl}.  All files for this mirror will be disabled until the next check.";
  #       close(SENDMAIL);
 
         next;
@@ -195,7 +195,7 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
 
 	foreach my $location (@locations) {
 
-		my $filepath = $location->{location_path};
+		my $filepath = $location->{path};
                 if (($filepath =~ m!/firefox/!)
                      && ($filepath !~ m!/namoroka/!)
                      && ($filepath !~ m!3\.6b1!)
@@ -210,27 +210,27 @@ while (my $mirror = $mirror_sth->fetchrow_hashref() ) {
 			$filepath =~ s@/en-US/@/tr/@;
 		}
 		log_this "Checking $filepath... ";
-		my $req = HTTP::Request->new(HEAD => $mirror->{mirror_baseurl} . $filepath);
+		my $req = HTTP::Request->new(HEAD => $mirror->{baseurl} . $filepath);
 		my $res = $ua->simple_request($req);
 
 		if (( $res->{_rc} == 200 ) && ( $res->{_headers}->{'content-type'} !~ /text\/html/ )) {
 			log_this "okay.\n";
-			$update_sth->execute($location->{location_id}, $mirror->{mirror_id}, '1');
+			$update_sth->execute($location->{id}, $mirror->{id}, '1');
 		}
 		else {
 			log_this "FAILED. rc=" . $res->{_rc} . "\n";
-			$update_sth->execute($location->{location_id}, $mirror->{mirror_id}, '0');
+			$update_sth->execute($location->{id}, $mirror->{id}, '0');
 		}
 
 		# content-type == text/plain hack here for Mac dmg's
 		if ($res->{_rc} == 200) {
                     foreach my $exten (keys %content_type) {
-			if ( $location->{location_path} =~ m/.*\.$exten$/ && $res->{_headers}->{'content-type'} !~ /\Q$content_type{$exten}\E/ ) {
+			if ( $location->{path} =~ m/.*\.$exten$/ && $res->{_headers}->{'content-type'} !~ /\Q$content_type{$exten}\E/ ) {
 				log_this " -> FAILED due to content-type mis-match, expected '$content_type{$exten}', got '$res->{_headers}->{'content-type'}'\n";
-	                    $update_sth->execute($location->{location_id}, $mirror->{mirror_id}, '0');
+	                    $update_sth->execute($location->{id}, $mirror->{id}, '0');
 			}
                     }
 		}
 	}
-        $log_sth->execute($start_timestamp, $mirror->{mirror_id}, '1', $mirror->{mirror_rating}, $output);
+        $log_sth->execute($start_timestamp, $mirror->{id}, '1', $mirror->{rating}, $output);
 }
