0be3a8ed18bb98266872fe3f3ae9825783a7c84b
Fred Wenzel <fwenzel@mozilla.com>
2010-01-13 09:19:40 -0800

    pointing download links on gonefishing page to releases.m.o (bug 464814, SVN r53428, p=justdave)

--- bouncer/php/gonefishing.php
+++ bouncer/php/gonefishing.php
@@ -8,9 +8,9 @@ require_once(HEADER);
 maintenance.  Please try this link later.</p>
 <p>During this maintenance, you may still download Mozilla products by visiting the following links:</p>
 <ul>
-<li><a href="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/">Mozilla Firefox</a></li>
-<li><a href="ftp://ftp.mozilla.org/pub/mozilla.org/thunderbird/">Mozilla Thunderbird</a></li>
-<li><a href="ftp://ftp.mozilla.org/pub/mozilla.org/">Other products...</a></li>
+<li><a href="http://releases.mozilla.org/pub/mozilla.org/firefox/releases/">Mozilla Firefox</a></li>
+<li><a href="http://releases.mozilla.org/pub/mozilla.org/thunderbird/">Mozilla Thunderbird</a></li>
+<li><a href="http://releases.mozilla.org/pub/mozilla.org/">Other products...</a></li>
 </ul>
 
 <?php
