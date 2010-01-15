58b101f4c46185f77e45d33edb32bc2044cbe699
Fred Wenzel <fwenzel@mozilla.com>
2010-01-15 08:45:15 -0800

    enabling markdown

--- settings.py
+++ settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.markup',
     'django.contrib.sessions',
     #'django.contrib.sites',
 )
