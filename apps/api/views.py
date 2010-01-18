f06bcdbabd7bba1d06a3d3859f842d4deb2f17a9
Fred Wenzel <fwenzel@mozilla.com>
2010-01-18 05:16:45 -0800

    subdirectories make me happy

--- apps/api/views.py
+++ apps/api/views.py
@@ -6,9 +6,10 @@ from django.template import RequestContext
 
 
 def _get_command_list():
-    templates = os.listdir(os.path.join(os.path.dirname(__file__), 'templates'))
+    templates = os.listdir(os.path.join(os.path.dirname(__file__), 'templates',
+                                        'docs'))
     # cut out the command names only
-    commands = [ t[8:-5] for t in templates if t.startswith('api_doc') ]
+    commands = [ t[:-5] for t in templates if t.endswith('.html') ]
     commands.sort()
     return commands
 
@@ -22,6 +23,6 @@ def docs(request, command):
     """Individual API docs"""
     if command not in _get_command_list():
         raise Http404
-    return render_to_response('api_doc_%s.html' % command, context_instance=
+    return render_to_response('docs/%s.html' % command, context_instance=
                               RequestContext(request))
 
