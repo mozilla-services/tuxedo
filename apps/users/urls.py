ccd72eb41443f4cad6a27e550e7b8c105b841da8
Fred Wenzel <fwenzel@mozilla.com>
2010-01-25 03:26:29 -0800

    password reset/change forms. wording like Django, but Bouncer-themed

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
-    (r'^login/', 'django.contrib.auth.views.login'),
-    (r'^logout/', 'django.contrib.auth.views.logout'),
-    (r'^profile/', 'users.views.profile'),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^profile/$', 'users.views.profile'),
+    (r'^profile/pwchange/$', 'django.contrib.auth.views.password_change'),
+    (r'^profile/pwchange/done/$',
+        'django.contrib.auth.views.password_change_done'),
+    (r'^profile/pwreset/$', 'django.contrib.auth.views.password_reset'),
+    (r'^profile/pwreset/done/$',
+        'django.contrib.auth.views.password_reset_done'),
+    (r'^profile/pwreset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm'),
+    (r'^profile/pwreset/complete/$',
+        'django.contrib.auth.views.password_reset_complete'),
 )
 
