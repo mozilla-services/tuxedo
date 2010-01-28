eaceed62aa3507514a1460966fd7c07fcd46aaff
Fred Wenzel <fwenzel@mozilla.com>
2010-01-28 10:52:19 -0800

    unit tests for product_show and _add (bug 542793)

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -1,23 +1,102 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+from xml.dom.minidom import parseString
 
-Replace these with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from mirror.models import Product
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+class APITestCase(TestCase):
+    """The mother of all API tests"""
+    def setUp(self):
+        # user and login
+        username = 'john'
+        pw = 'johnpw'
+
+        self.user = User.objects.create_user(
+            username, 'lennon@thebeatles.com', pw
+        )
+        self.user.is_staff = True
+        self.user.save()
+        self.c = Client()
+        self.c.login(username=username, password=pw)
+
+    def tearDown(self):
+        self.user.delete()
+
+# TODO uptake test
+
+class ProductTest(APITestCase):
+    def setUp(self):
+        super(ProductTest, self).setUp()
+
+        # products
+        self.products = []
+        for i in range(1, 11):
+            name = 'Product-%s-%s' % (i, i%2 and 'odd' or 'even')
+            p = Product(name=name)
+            p.save()
+            self.products.append(p)
+
+    def tearDown(self):
+        super(ProductTest, self).tearDown()
+        for p in self.products:
+            p.delete()
+
+    def test_product_show_all(self):
+        """Make sure all products show up"""
+        response = self.c.get(reverse('api.views.product_show'))
+        xmldoc = parseString(response.content)
+        prods = xmldoc.getElementsByTagName('product')
+        self.assertEqual(len(prods), len(self.products),
+                         'need to return all products')
+
+    def test_product_show_partial(self):
+        """Make sure partial product list works"""
+        response = self.c.get(reverse('api.views.product_show'),
+                              {'product': 'odd'})
+        xmldoc = parseString(response.content)
+        prods = xmldoc.getElementsByTagName('product')
+        self.assertEqual(len(prods), len(self.products)/2,
+                         'need to return partial product list')
+
+    def test_product_add_new(self):
+        """Add a new product through the API"""
+        new_prod = "Firefox"
+
+        response = self.c.post(reverse('api.views.product_add'),
+                               {'product': new_prod})
+        xmldoc = parseString(response.content)
+
+        all_products = Product.objects.all()
+        self.assertEqual(len(all_products), len(self.products)+1,
+                        'new product as added')
+
+        prods = xmldoc.getElementsByTagName('product')
+        self.assertEqual(len(prods), 1, 'one product added and returned')
+        self.assertTrue(int(prods[0].getAttribute('id')) > 0,
+                        'new product id returned')
+        self.assertEqual(prods[0].childNodes[0].data, new_prod,
+                        'product name returned')
+
+    def test_product_add_existing(self):
+        """Adding an existing product will be handled gracefully"""
+        new_prod = self.products[0].name
+
+        response = self.c.post(reverse('api.views.product_add'),
+                               {'product': new_prod})
+        xmldoc = parseString(response.content)
+
+        all_products = Product.objects.all()
+        self.assertEquals(len(all_products), len(self.products),
+                         'existing product not re-added')
+
+        prods = xmldoc.getElementsByTagName('product')
+        self.assertEqual(len(prods), 1, 'one product returned')
+        self.assertEqual(int(prods[0].getAttribute('id')), self.products[0].id,
+                        'existing product id returned')
+        self.assertEqual(prods[0].childNodes[0].data, new_prod,
+                          'product name returned')
 
