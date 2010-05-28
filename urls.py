d64fbdc689da72547c8e8a457df702284c9f5f4f
Fred Wenzel <fwenzel@mozilla.com>
2010-05-28 08:39:35 -0700

    added a 404 and 500 handler, and made Debug = False by default.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, handler404, handler500
 from django.contrib import admin
 
 admin.autodiscover()
