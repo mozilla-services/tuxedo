5e1710205b3956177d88dc2022097d6b07653e08
Fred Wenzel <fwenzel@mozilla.com>
2010-09-07 15:46:03 -0700

    Show inline list of mirrors when editing a region. Bug 580829.

--- apps/geoip/admin.py
+++ apps/geoip/admin.py
@@ -1,5 +1,7 @@
-from geoip.models import Country, IPBlock, Region
 from django.contrib import admin
+
+from geoip.models import Country, IPBlock, Region
+from mirror.models import Mirror
 
 
 class CountryAdmin(admin.ModelAdmin):
@@ -8,6 +10,7 @@ class CountryAdmin(admin.ModelAdmin):
     ordering = ('country_name',)
 admin.site.register(Country, CountryAdmin)
 
+
 class IPBlockAdmin(admin.ModelAdmin):
     list_display = ('ip_start_addr', 'ip_start', 'ip_end_addr', 'ip_end',
                     'country')
@@ -15,9 +18,14 @@ class IPBlockAdmin(admin.ModelAdmin):
     ordering = ('ip_start',)
 admin.site.register(IPBlock, IPBlockAdmin)
 
+
+class MirrorInline(admin.TabularInline):
+    model = Mirror.regions.through
+
+
 class RegionAdmin(admin.ModelAdmin):
+    inlines = (MirrorInline,)
     list_display = ('name', 'mirror_count', 'ratings', 'priority', 'throttle')
     list_filter = ('priority',)
     ordering = ('name',)
 admin.site.register(Region, RegionAdmin)
-
