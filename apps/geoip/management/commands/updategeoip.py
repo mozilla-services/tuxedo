0e180a27c192acd56120dc7d88445509388cb898
Anthony Ricaud <anthony@ricaud.me>
2011-09-23 13:06:41 +0200

    Bug 677640 - Use Django ORM and add an arg check

--- apps/geoip/management/commands/updategeoip.py
+++ apps/geoip/management/commands/updategeoip.py
@@ -1,7 +1,7 @@
 import csv
 
-from django.core.management.base import BaseCommand
-from django.db import transaction, connection
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 
 from geoip.models import IPBlock, Country
 
@@ -14,12 +14,13 @@ class Command(BaseCommand):
 
     @transaction.commit_on_success
     def handle(self, *args, **options):
-        cursor = connection.cursor()
-        cursor.execute("DELETE FROM %s" % IPBlock._meta.db_table)
+        if len(args) is not 1:
+            raise CommandError('You need one file_path argument')
 
+        IPBlock.objects.all().delete()
         with open(args[0]) as f:
             for row in csv.reader(f):
                 ip_block = IPBlock(ip_start=row[2],
-                                ip_end=row[3],
+                                   ip_end=row[3],
                                    country=Country(row[4]))
                 ip_block.save()
