bd57d7424239ee13b2eb9e46c05904025f4c934c
Anthony Ricaud <anthony@ricaud.me>
2011-09-24 00:49:27 +0200

    Bug 656046 - Need to be able to edit IP blocks in the GeoIP database using normal IP addresses

--- apps/geoip/admin.py
+++ apps/geoip/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from geoip.models import Country, IPBlock, Region
 from mirror.models import Mirror
@@ -11,11 +12,41 @@ class CountryAdmin(admin.ModelAdmin):
 admin.site.register(Country, CountryAdmin)
 
 
+# Inspired by http://www.hindsightlabs.com/blog/2010/02/11/adding-extra-fields-to-a-model-form-in-djangos-admin/
+class IPBlockFormAdmin(forms.ModelForm):
+    ip_start_addr = forms.CharField()
+    ip_end_addr = forms.CharField()
+
+    class Meta:
+        model = IPBlock
+
+    def __init__(self, *args, **kwargs):
+        super(IPBlockFormAdmin, self).__init__(*args, **kwargs)
+
+        if 'instance' in kwargs:
+            instance = kwargs['instance']
+            self.initial['ip_start_addr'] = instance.ip_start_addr
+            self.initial['ip_end_addr'] = instance.ip_end_addr
+
+    def save(self, commit=True):
+        model = super(IPBlockFormAdmin, self).save(commit=False)
+
+        model.ip_start_addr = self.cleaned_data['ip_start_addr']
+        model.ip_end_addr = self.cleaned_data['ip_end_addr']
+
+        if commit:
+            model.save()
+
+        return model
+
+
 class IPBlockAdmin(admin.ModelAdmin):
+    form = IPBlockFormAdmin
     list_display = ('ip_start_addr', 'ip_start', 'ip_end_addr', 'ip_end',
                     'country')
     list_filter = ('country',)
     ordering = ('ip_start',)
+    fields = ('ip_start_addr', 'ip_end_addr', 'country')
 admin.site.register(IPBlock, IPBlockAdmin)
 
 
