a573760560f72934ed2671ccd7a24e1c940d5033
Anthony Ricaud <anthony@ricaud.me>
2012-03-20 18:12:19 +0100

    Add profile edition

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,18 +1,20 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User as DjangoUser
+from django.http import HttpResponseForbidden
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from forms import DjangoUserForm, UserProfileForm
 
 
 @login_required
-def profile(request):
+def profile(request, user):
     data = {}
 
     if request.POST:
-        django_user_form = DjangoUserForm(request.POST, instance=request.user)
+	django_user_form = DjangoUserForm(request.POST, instance=user)
         user_profile_form = UserProfileForm(request.POST,
-                                            instance=request.user.get_profile())
+					    instance=user.get_profile())
 
         if django_user_form.is_valid() and user_profile_form.is_valid():
             django_user_form.save()
@@ -20,8 +22,8 @@ def profile(request):
             data['success'] = True
 
     else:
-        django_user_form = DjangoUserForm(instance=request.user)
-        user_profile_form = UserProfileForm(instance=request.user.get_profile())
+	django_user_form = DjangoUserForm(instance=user)
+	user_profile_form = UserProfileForm(instance=user.get_profile())
 
     data.update({
         'django_user_form': django_user_form,
@@ -31,3 +33,15 @@ def profile(request):
     return render_to_response('registration/profile.html', data,
                               context_instance=RequestContext(request))
 
+
+@login_required
+def own_profile(request):
+    return profile(request, request.user)
+
+
+@permission_required('users.change_user')
+def profile_edition(request, username):
+    django_user = get_object_or_404(DjangoUser, username=username)
+    if django_user.is_staff:
+	return HttpResponseForbidden(u'You don\'t have the permission to edit this user')
+    return profile(request, django_user)
