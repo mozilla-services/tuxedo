c72dbf4288292169aa26a8a9a711bbfb836fc17b
Brandon Savage <brandon@brandonsavage.net>
2012-09-21 16:41:23 -0400

    Updating the settings-dist.py file to reflect upgrade changes between Django 1.2 and 1.4.

--- settings-dist.py
+++ settings-dist.py
@@ -71,7 +71,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin-media/'
+STATIC_URL = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '### change me ###'
@@ -80,6 +80,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 )
 
