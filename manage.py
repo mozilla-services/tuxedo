54a33a55ca7f11b6acc3c354aaf2cde7162ba3b8
Jeremiah Orem <oremj@oremj.com>
2018-12-14 07:45:37 -0800

    django 2 manage.py

--- manage.py
+++ manage.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
-import site
+#!/usr/bin/env python3
+import os
+import sys
 
-from django.core.management import execute_manager
-
-
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write(
-        "Error: Can't find the file 'settings.py' in the directory containing "
-        "%r. It appears you've customized things.\nYou'll have to run "
-        "django-admin.py, passing it your settings module.\n(If the file "
-        "settings.py does indeed exist, it's causing an ImportError "
-        "somehow.)\n" % __file__)
-    sys.exit(1)
-
-
-site.addsitedir(settings.path('apps'))
-
-
-if __name__ == "__main__":
-    execute_manager(settings)
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tux2.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
